58b75612edafdb33fbf716dc5c0aa32d8f5b672a
Thomas Munro <macdice@penski.net>
2013-11-17 20:55:31 +0000

    Added login handler.

--- server/django_project/scapi1/views.py
+++ server/django_project/scapi1/views.py
@@ -1,6 +1,44 @@
 from django.db import connection
 from django.http import HttpResponse
+from django.shortcuts import render
 import json
+import uuid
+
+def docs(request):
+    return render(request, "docs.html", { "test": "foox" })
+
+def login(request):
+    cursor = connection.cursor()
+    cursor.execute("""SELECT id, firstname, lastname, email, status
+                        FROM person
+                       WHERE username = %s
+                         AND password_hash = crypt(%s, password_hash)""",
+                  (request.POST["username"], request.POST["password"]))
+    row = cursor.fetchone()
+    if row == None:
+        # TODO count failures, implement back-off
+        response = { "status": "FAIL" }
+    else:
+        id, firstname, lastname, email, status = row        
+        if status == "ACTIVE":
+            token = uuid.uuid4().hex
+            cursor.execute("""INSERT INTO login_session (token, person, created)
+                              VALUES (%s, %s, now())""",
+                           (token, id))
+            cursor.execute("""UPDATE person
+                                 SET last_login = now(),
+                                     logins = logins + 1
+                               WHERE id = %s""",
+                           (id,))
+            response = { "status" : "OK",
+                         "firstname" : firstname,
+                         "lastname" : lastname,
+                         "email" : email,
+                         "token" : token }
+        else:
+            response = { "status" : "FAIL" }
+            # TODO handle this case
+    return HttpResponse(json.dumps(response, indent=4), content_type="application/json")
 
 def schedule(request, username):
     cursor = connection.cursor()
