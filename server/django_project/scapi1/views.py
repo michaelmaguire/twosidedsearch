58ee1ece0219d16578a1cbe40a8e203e9354d53a
Thomas Munro <macdice@penski.net>
2014-01-04 00:37:16 +0000

    First hack at the search-on-search system.

--- server/django_project/scapi1/views.py
+++ server/django_project/scapi1/views.py
@@ -9,146 +9,236 @@ def json_response(object):
     return HttpResponse(json.dumps(object, indent=4),
                         content_type="application/json")
 
+def begin(request):
+    """The prelude to be used by all view functions to obtain or
+    create a profile ID using the device ID information present in the
+    request."""
+    # in development, we use a header or a parameter, but later we
+    # will use some fancy-pants certificate stuff
+    device_id = request.REQUEST["x-id"]
+    # this could be someone we've never heard of, or someone returning
+    profile_id = None
+    cursor = connection.cursor()
+    # note that this actually serializes all access for a given device
+    # since it locks the device row.  is that a bad thing?  could do
+    # better if necessary, not real need to update that last_seen
+    # thing for every request anyway
+    cursor.execute("""UPDATE speedycrew.device
+                         SET last_seen = now()
+                       WHERE id = %s
+                   RETURNING profile""",
+                   (device_id, ))
+    if cursor.rowcount == 1:
+        profile_id = cursor.fetchone()[0]
+    else:
+        # new device ID, so we create a new profile
+        cursor.execute("""INSERT INTO speedycrew.profile (id, status, created) 
+                          VALUES (DEFAULT, 'ACTIVE', now())
+                       RETURNING id""",
+                       (device_id, ))
+        profile_id = cursor.fetchone()[0]
+        cursor.execute("""INSERT INTO speedycrew.device (id, profile, last_seen, created) 
+                          VALUES (%s, %s, now(), now())""",
+                       (device_id, profile_id))
+    cursor.close()
+    return profile_id
+
 def docs(request):
+    """A view handler for showing the documentation/test interface."""
+    # TODO turn this shit off in production
     return render(request, "docs.html", { "test": "foox" })
 
-def create_account(request):
+def profile(request):
+    """A view handler for fetching the user's profile data."""
+    profile_id = begin(request)
     cursor = connection.cursor()
-    # TODO reject empty names etc
-    # TODO check if the password is not good enough according to some policy
-    # TODO likewise for the username
+    cursor.execute("""SELECT p.username,
+                             p.real_name,
+                             p.email,
+                             p.status,
+                             p.message,
+                             p.created
+                        FROM speedycrew.profile p
+                       WHERE p.id = %s""",
+                   (profile_id, ))
+    username, real_name, email, status, message, created = cursor.fetchone()
+    return json_response({ "username" : username,
+                           "real_name" : real_name,
+                           "email" : email,
+                           "status" : status,
+                           "message" : message,
+                           "created" : created.isoformat() })
 
-    # the following checks are 'polite' ways to discover emails and
-    # username already in use and give a friendly error message:
-    # obviously there is also a race here due to concurrency, and the
-    # database will enforce uniqueness (the error will just be uglier
-    # if we get there); in theory you'd just use the DB and detect
-    # those errors specially, a job for another day
-    
-    # check if the email address is already known to us
-    cursor.execute("""SELECT 1
-                        FROM person
-                       WHERE email = %s""",
-                   (request.POST["email"],))
-    if cursor.fetchone():
-        return json_response({ "status" : "EMAIL_IN_USE" })
-
-    # check if the email address is already known to us
-    cursor.execute("""SELECT 1
-                        FROM person
-                       WHERE username = %s""",
-                   (request.POST["username"],))
-    if cursor.fetchone():
-        return json_response({ "status" : "USERNAME_IN_USE" })
-    
-    # clear to proceed
-    cursor.execute("""INSERT INTO person (username, firstname, lastname, email, status, password_hash, created, logins)
-                      VALUES (%s, %s, %s, %s, 'ACTIVE', crypt(%s, gen_salt('bf')), now(), 0)""",
-                   (request.POST["username"],
-                    request.POST["firstname"],
-                    request.POST["lastname"],
-                    request.POST["email"],
-                    request.POST["password"]))
+def update_profile(request):
+    profile_id = begin(request)
+    cursor = connection.cursor()
+    real_name = param_or_null(request, "real_name")
+    email = param_or_null(request, "email")
+    message = param_or_null(request, "message")
+    cursor.execute("""UPDATE speedycrew.profile
+                         SET real_name = %s,
+                             email = %s,
+                             message = %s,
+                             modified = now()
+                       WHERE id = %s""",
+                   (real_name, email, message, profile_id))
     return json_response({ "status" : "OK" })
 
-def login(request):
+def searches(request):
+    """A view handler that returns a summary of the user's currently
+    active searches."""
+    profile_id = begin(request)
     cursor = connection.cursor()
-    cursor.execute("""SELECT id, firstname, lastname, email, status
-                        FROM person
-                       WHERE username = %s
-                         AND password_hash = crypt(%s, password_hash)""",
-                  (request.POST["username"], request.POST["password"]))
-    row = cursor.fetchone()
-    if row == None:
-        # TODO count failures, implement back-off
-        return json_response({ "status": "FAIL" })
-        
-    id, firstname, lastname, email, status = row        
-    if status == "ACTIVE":
-        token = uuid.uuid4().hex
-        cursor.execute("""INSERT INTO login_session (token, person, created)
-                          VALUES (%s, %s, now())""",
-                       (token, id))
-        cursor.execute("""UPDATE person
-                             SET last_login = now(),
-                                 logins = logins + 1
-                           WHERE id = %s""",
-                       (id,))
-        return json_response({ "status" : "OK",
-                               "firstname" : firstname,
-                               "lastname" : lastname,
-                               "email" : email,
-                               "token" : token })
-    else:
-        # TODO handle this case better (say 'you're banned'?)
-        return json_response({ "status" : "FAIL" })
+    cursor.execute("""SELECT s.id, s.name, s.side, s.created
+                        FROM speedycrew.search s
+                       WHERE s.owner = %s
+                       ORDER BY s.name, s.created""",
+                   (profile_id, ))
+    searches = []
+    for id, name, side, created in cursor:
+        searches.append({ "id" : id,
+                          "name" : name,
+                          "side" : side,
+                          "created" : created.isoformat() })
+    return json_response({ "status" : "OK",
+                           "searches" : searches })
 
-def logout(request):
+def tags(request):
+    """A view handler for retrieving tag names, suitable for
+    auto-completion."""
+    profile_id = begin(request)
+    prefix = request.REQUEST["prefix"]
+    # TODO sanitise?    
     cursor = connection.cursor()
-    cursor.execute("""UPDATE login_session
-                         SET ended = now()
-                       WHERE token = %s
-                         AND ended IS NULL""",
-                   (request.POST["token"],))
+    # TODO should probably try to suggest popular tags
+    cursor.execute("""SELECT t.name
+                        FROM speedycrew.tag t
+                       WHERE t.name LIKE %s || '%%'
+                         AND t.status = 'ACTIVE'
+                    ORDER BY t.name
+                       LIMIT 20""",
+                   (prefix, ))
+    results = []
+    for name, in cursor:
+        results.append(name)
+    return json_response({ "status" : "OK",
+                           "tags" : results })
+
+def param_or_null(request, name):
+    if name not in request.REQUEST:
+        return None
+    else:
+        return request.REQUEST[name]
+
+def create_search(request):
+    """A view handler to create a new search."""
+    profile_id = begin(request)
+    cursor = connection.cursor()
+
+    # required parameters
+    tags = request.REQUEST["tags"].split(",")
+    side = request.REQUEST["side"]
+    longitude = request.REQUEST["longitude"]
+    latitude = request.REQUEST["latitude"]
+
+    # optional parameters
+    name = param_or_null(request, "name")
+    address = param_or_null(request, "address")
+    city = param_or_null(request, "city")
+    country = param_or_null(request, "country")
+    postcode = param_or_null(request, "postcode")
+    radius = param_or_null(request, "radius") # required if side = PROVIDE
+
+    # resolve tags to tag IDs, creating this if necessary
+    tag_ids = []
+    for tag in tags:
+        cursor.execute("""SELECT id, status
+                            FROM speedycrew.tag
+                           WHERE name = %s""",
+                       (tag,))
+        row = cursor.fetchone()
+        if row == None:
+            cursor.execute("""INSERT INTO speedycrew.tag (id, name, status, created, creator)
+                              VALUES (DEFAULT, %s, 'ACTIVE', now(), %s)
+                           RETURNING id""",
+                           (tag, profile_id))
+            tag_id = cursor.fetchone()[0]
+            tag_ids.append(tag_id)
+        else:
+            tag_id, tag_status = row
+            if tag_status != "ACTIVE":
+                return json_response({ "status" : "TAG_BLOCKED",
+                                       "blocked_tag" : tag,
+                                       "message" : "tag is not allowed" })
+            tag_ids.append(tag_id)
+        
+    cursor.execute("""INSERT INTO speedycrew.search (id, owner, name, side, address, postcode, city, country, geography, radius, status, created)
+                      VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, speedycrew.make_geo(%s, %s), %s, 'ACTIVE', now())
+                   RETURNING id""",
+                   (profile_id, name, side, address, postcode, city, country, longitude, latitude, radius))
+    search_id = cursor.fetchone()[0]
+    for tag_id in tag_ids:
+        cursor.execute("""INSERT INTO speedycrew.search_tag VALUES (%s, %s)""",
+                       (search_id, tag_id))
+
+    # TODO since the user is waiting, do some kind of limited version
+    # of run_search synchronously?
+    cursor.execute("""SELECT speedycrew.run_search(%s)""", (search_id, ))
+
+    # TODO feed some actual responses back?  that'd be friendly.  for
+    # now, here, take a number, go and get the results with another
+    # request!
+    return json_response({ "status" : "OK",
+                           "search_id" : search_id })
+
+def delete_search(request):
+    """End an existing active search."""
+    profile_id = begin(request)
+    search_id = request.REQUEST["search"]
+    cursor = connection.cursor()
+    cursor.execute("""UPDATE speedycrew.search
+                         SET status = 'DELETED'
+                       WHERE id = %s
+                         AND owner = %s
+                         AND status = 'ACTIVE'""",
+                   (search_id, profile_id))
     if cursor.rowcount == 1:
         return json_response({ "status" : "OK" })
     else:
-        return json_response({ "status" : "FAIL" })
+        return json_response({ "status" : "ERROR" })
 
-def schedule(request, username):
+def search_results(request):
+    """A dumb request for all results for a given search ID."""
+    profile_id = begin(request)
+    search_id = request.REQUEST["search"]
     cursor = connection.cursor()
-    cursor.execute("""SELECT pd.day, pd.availability, pd.note
-                        FROM person p
-                        JOIN person_day pd ON p.id = pd.person
-                       WHERE p.username = %s
-                       ORDER BY pd.day""",
-                   (username,)) 
-    response = {}
-    response["status"] = "OK"
-    response["username"] = username
-    response["data"] = [ { "day" : day.isoformat(), 
-                           "availability" : availability, 
-                           "note" : note } 
-                         for day, availability, note in cursor.fetchall() ]
-    cursor.close()
-    return json_response(response)
-
-def find(request):
-    # TODO require valid token, even though we do nothing with your
-    # identity here
-    cursor = connection.cursor()
-    cursor.execute(""" SELECT p.username,
-                              p.firstname,
-                              p.lastname,
-                              p.email,
-                              p.message,
-                              st_distance(l.geography,
-                                          st_geographyfromtext('POINT(' || %s || ' ' || %s || ')'))
-                         FROM person p
-                         JOIN person_day pd ON p.id = pd.person
-                         JOIN person_skill ps ON p.id = ps.person
-                         JOIN skill s ON ps.skill = s.id
-                         JOIN travel_area ta ON p.id = ta.person
-                         JOIN location l ON ta.base = l.id           
-                        WHERE pd.availability = 'AVAILABLE'
-                          AND pd.day = %s::DATE
-                          AND s.name = %s
-                          AND st_dwithin(l.geography,
-                                         st_geographyfromtext('POINT(' || %s || ' ' || %s || ')'),
-                                         ta.max_distance)
-                        ORDER BY p.firstname, p.lastname""",
-                   (request.POST["longitude"],
-                    request.POST["latitude"],
-                    request.POST["day"],
-                    request.POST["skill"],
-                    request.POST["longitude"],
-                    request.POST["latitude"]))
+    cursor.execute("""SELECT s2.id, 
+                             p.username, 
+                             p.real_name, 
+                             p.email, 
+                             s2.address, 
+                             s2.postcode, 
+                             s2.city, 
+                             s2.country, 
+                             st_distance(s1.geography, s2.geography) AS distance
+                        FROM speedycrew.match m
+                        JOIN speedycrew.search s1 ON m.a = s1.id
+                        JOIN speedycrew.search s2 ON m.b = s2.id
+                        JOIN speedycrew.profile p ON s2.owner = p.id
+                       WHERE s1.id = %s
+                         AND s1.owner = %s
+                         AND s2.status = 'ACTIVE'""",
+                   (search_id, profile_id))
+    results = []
+    for id, username, real_name, email, address, postcode, city, country, distance in cursor:
+        results.append({ "id" : id,
+                         "username" : username,
+                         "real_name" : real_name,
+                         "email" : email,
+                         "address" : address,
+                         "postcode" : postcode,
+                         "city" : city,
+                         "country" : country,
+                         "distance" : distance })
     return json_response({ "status" : "OK",
-                           "data" : [ { "username" : username,
-                                        "firstname" : firstname,
-                                        "lastname" : lastname,
-                                        "email" : email,
-                                        "message" : message,
-                                        "distance" : distance }
-                                      for username, firstname, lastname, email, message, distance
-                                      in cursor.fetchall() ] })
+                           "results" : results })                        
