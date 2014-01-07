c28a4f47a0fe1851a059c8f0a3f7ee9346e07588
Thomas Munro <macdice@penski.net>
2014-01-07 21:06:21 +0000

    Oops -- added SUM.

--- server/scripts/maintain_tag_stats.py
+++ server/scripts/maintain_tag_stats.py
@@ -8,8 +8,8 @@ def run(connection):
     cursor.execute("""DELETE FROM speedycrew.tag_count""")
     cursor.execute("""INSERT INTO speedycrew.tag_count (tag, provide_counter, seek_counter, counter)
                       SELECT st.tag,
-                             CASE s.side WHEN 'PROVIDE' THEN 1 ELSE 0 END,
-                             CASE s.side WHEN 'SEEK' THEN 1 ELSE 0 END,
+                             SUM(CASE s.side WHEN 'PROVIDE' THEN 1 ELSE 0 END),
+                             SUM(CASE s.side WHEN 'SEEK' THEN 1 ELSE 0 END),
                              COUNT(*)
                         FROM speedycrew.search s
                         JOIN speedycrew.search_tag st ON s.id = st.search
