305e2f21180121cf8c4606ba35ab97f966dce32a
Dietmar Kühl <dietmar.kuehl@gmail.com>
2014-05-18 21:24:35 +0100

    changed the code to use HTTP POST instead of HTTP GET.

--- client/ios/SpeedyCrew/SpCData.mm
+++ client/ios/SpeedyCrew/SpCData.mm
@@ -25,8 +25,8 @@
     SpCDatabase* database = [SpCDatabase database];
     self.baseURL = @"http://captain:cook@dev.speedycrew.com/api/1/";
     self.identity = [database querySetting: @"scid"];
-    NSString* query = [NSString stringWithFormat:@"profile?x-id=%@", self.identity];
-    [self sendHttpRequest: query];
+    NSString* query = [NSString stringWithFormat:@"x-id=%@", self.identity];
+    [self sendHttpRequest:@"profile" withBody:query];
     
     self.searches = [[NSMutableArray alloc] init]; //-dk:TODO recover stored searches
     self.listeners = [[NSMutableDictionary alloc] init];
@@ -50,26 +50,26 @@
     SpCDatabase* database = [SpCDatabase database];
     [database updateSetting:name with:value];
     NSString* encoded = [value stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
-    NSString* query = [NSString stringWithFormat:@"update_profile?x-id=%@&%@=%@", self.identity, name, encoded];
-    [self sendHttpRequest: query];
+    NSString* query = [NSString stringWithFormat:@"x-id=%@&%@=%@", self.identity, name, encoded];
+    [self sendHttpRequest:@"update_profile" withBody:query];
 }
 
 - (void)updateSearches
 {
-    NSString* query = [NSString stringWithFormat:@"searches?x-id=%@", self.identity];
-    [self sendHttpRequest: query];
+    NSString* query = [NSString stringWithFormat:@"x-id=%@", self.identity];
+    [self sendHttpRequest:@"searches" withBody:query];
 }
 
 - (void)addSearchWithText:(NSString*)text forSide:(NSString*)side
 {
     //-dk:TODO get the radius from the configuration
-    NSString* query = [NSString stringWithFormat:@"create_search?x-id=%@&side=%@%@&query=%@&longitude=-0.15&latitude=51.5",
+    NSString* query = [NSString stringWithFormat:@"x-id=%@&side=%@%@&query=%@&longitude=-0.15&latitude=51.5",
                         self.identity,
                         side,
                         [side isEqual:@"SEEK"]? @"&radius=5000": @"",
                         [SpCData encodeURL:text]
                        ];
-    [self sendHttpRequest: query];
+    [self sendHttpRequest:@"create_search" withBody: query];
 }
 
 - (void)receivedResponse:(NSData*)data
@@ -92,7 +92,8 @@
                     NSString* query = [dict objectForKey: @"query"];
                     if (id && side && query) {
                         NSString* sid = [NSString stringWithFormat:@"%@", id];
-                        [self sendHttpRequest: [NSString stringWithFormat:@"search_results?x-id=%@&search=%@&request_id=%@", self.identity, id, id]];
+                        [self sendHttpRequest:@"search_results"
+                                     withBody:[NSString stringWithFormat:@"x-id=%@&search=%@&request_id=%@", self.identity, id, id]];
                         std::string insert("insert into searches(id, side, search) values("
                                            "'" + db->escape([sid UTF8String]) + "', "
                                            "'" + db->escape([side UTF8String]) + "', "
@@ -136,7 +137,7 @@
                 }
             }
             else if ([type isEqual:@"create_search_response"]) {
-                [self sendHttpRequest: [NSString stringWithFormat:@"searches?x-id=%@", self.identity]];
+                [self sendHttpRequest: @"searches" withBody:[NSString stringWithFormat:@"x-id=%@", self.identity]];
             } 
             else {
                 NSLog(@"unprocessed message type='%@'", type);
@@ -154,9 +155,30 @@
 - (void)sendHttpRequest:(NSString*)query
 {
     NSString* str = [NSString stringWithFormat:@"%@%@", self.baseURL, query];
-    NSLog(@"sending Http Request: '%@'", str);
+    NSLog(@"sending HTTP GET: url='%@'", str);
     NSURL* url = [NSURL URLWithString:str];
     NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    NSOperationQueue* q = [NSOperationQueue mainQueue];
+    [NSURLConnection sendAsynchronousRequest:request queue:q completionHandler:
+     ^(NSURLResponse* resp, NSData* d, NSError* err) {
+         if (d) {
+             [self receivedResponse:d];
+         }
+         else {
+             NSLog(@"received error: %@", [err localizedDescription]);
+         }
+     }];
+}
+
+- (void)sendHttpRequest:(NSString*)query withBody:(NSString*)body
+{
+    NSString* str = [NSString stringWithFormat:@"%@%@", self.baseURL, query];
+    NSLog(@"sending HTTP POST: url='%@' body='%@'", str, body);
+    NSURL* url = [NSURL URLWithString:str];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     NSOperationQueue* q = [NSOperationQueue mainQueue];
     [NSURLConnection sendAsynchronousRequest:request queue:q completionHandler:
      ^(NSURLResponse* resp, NSData* d, NSError* err) {
