65ac644acb5315e90b4f1086db125d1a464e4063
Dietmar Kühl <dietmar.kuehl@gmail.com>
2014-05-25 18:57:12 +0100

    started to integrate a map

---
+++ client/ios/SpeedyCrew/SpCMapViewController.h
@@ -0,0 +1,14 @@
+//
+//  SpCMapViewController.h
+//  SpeedyCrew
+//
+//  Created by Dietmar Kühl on 18/05/2014.
+//  Copyright (c) 2014 Dietmar Kühl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpCMapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UINavigationItem *backButton;
+
+@end
