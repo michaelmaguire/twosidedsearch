5d17900bf3007d6db87c5901268454d1169051ef
Michael Maguire <michael.maguire@gmail.com>
2014-06-17 03:07:22 +0100

    Working retrieval and processing of synchronise [sic] data

---
+++ client/android/main/src/com/speedycrew/client/sql/Control.java
@@ -0,0 +1,9 @@
+package com.speedycrew.client.sql;
+
+public interface Control {
+	public final static String TABLE_NAME = "control";
+
+	// Columns names
+	public static final String TIMELINE = "timeline";
+	public static final String SEQUENCE = "sequence";
+}
