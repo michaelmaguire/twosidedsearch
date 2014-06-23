50c214cd28a2270b7dcaa4888ba6f8ad56a32e5e
Thomas Munro <munro@ip9.org>
2014-06-23 21:27:05 +0100

    Script for dumping schema into create.sql

---
+++ server/db/dump_schema.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DB="$1"
+if [ "$DB" ==  "" ] ; then
+  DB=speedy_hack
+fi
+pg_dump --schema-only --no-owner --no-privileges --no-tablespaces -U speedycrew $DB > create.sql && \
+pg_dump --data-only --column-inserts --table=speedycrew.schema_change -U speedycrew $DB >> create.sql
