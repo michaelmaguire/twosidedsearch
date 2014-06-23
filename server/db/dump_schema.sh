dc6151bec01f0944db3ff7166038d00a29931c65
Thomas Munro <munro@ip9.org>
2014-06-23 20:38:02 +0000

    Exclude non-speedycrew schemas

--- server/db/dump_schema.sh
+++ server/db/dump_schema.sh
@@ -3,5 +3,5 @@ DB="$1"
 if [ "$DB" ==  "" ] ; then
   DB=speedy_hack
 fi
-pg_dump --schema-only --no-owner --no-privileges --no-tablespaces -U speedycrew $DB > create.sql && \
+pg_dump --schema-only --no-owner --no-privileges --no-tablespaces --schema=speedycrew -U speedycrew $DB > create.sql && \
 pg_dump --data-only --column-inserts --table=speedycrew.schema_change -U speedycrew $DB >> create.sql
