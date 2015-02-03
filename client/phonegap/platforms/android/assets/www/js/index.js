f18f8509be931667053e14361f12310adeacf4dd
Michael Maguire <bitbucket@michaelmaguire.ca>
2015-02-03 22:51:42 +0000

    Generated result of calling 'phonegap build android'

--- client/phonegap/platforms/android/assets/www/js/index.js
+++ client/phonegap/platforms/android/assets/www/js/index.js
@@ -33,6 +33,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
+        alert("I am a test alert! Hello World");
+
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
