0750cb87a7f9448e4cfd8e19351712a97a9227e1
Michael Maguire <github@michaelmaguire.ca>
2015-02-07 00:47:47 +0000

    Rad 'phonegap build ios'

--- client/phonegap/platforms/ios/www/js/index.js
+++ client/phonegap/platforms/ios/www/js/index.js
@@ -18,35 +18,39 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
+        "use strict";
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
+        "use strict";
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
+        "use strict";
 
-        alert("I am a test alert! Hello World");
+        window.alert("I am a test alert! Hello World");
 
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function (id) {
+        "use strict";
+        var parentElement = document.getElementById(id),
+            listeningElement = parentElement.querySelector('.listening'),
+            receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        window.console.log('Received Event: ' + id);
     }
 };
